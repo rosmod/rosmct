70f30790f8023aa59b83b90ff5ed6708f9efa727
Dwatkins <dexter.a.watkins@vanderbilt.edu>
2017-11-07 11:48:05 -0600

    roslib test and plugin src files

---
+++ src/plugin.js
@@ -0,0 +1,19 @@
+define([
+    "./src/rosmctServerAdapter.js",
+    "./src/rosmctObjectProvider.js",
+    "./src/rosmctCompositionProvider.js",
+    "./src/rosmctTelemetryProvider.js"
+], function(
+    ServerAdapter,
+    ObjectProvider,
+    CompositionProvider,
+    TelemetryProvider
+){
+    return function(){
+        return function(openmct){
+
+            //plugin installation here
+
+        };
+    };
+});
